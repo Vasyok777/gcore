e1bfa1bb555a354e6e72978165d66ce3c36876fd
Vasyl Holub <vasya.holub.bf@gmail.com>
2024-05-29 20:28:04 +0300

    versin 11

--- src/components/OtherProducts/OtherProducts.tsx
+++ src/components/OtherProducts/OtherProducts.tsx
@@ -35,7 +35,7 @@ const OtherProducts = () => {
             scrub: true,
             start: "top 95%",
             end: "50% 20%",
-            markers: true,
+            // markers: true,
           },
         },
       )
